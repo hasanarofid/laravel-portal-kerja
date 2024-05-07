2ecef0ee1e2c78e0c7629631f41c3d79b22858ad
Andrean Ricky <andreansetyaji@gmail.com>
2024-05-07 21:03:07 +0700

    Permintaan Mahrus Menambah user pencaker

---
+++ database/migrations/2024_05_07_135930_user_pencaker.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserPencaker extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_pencaker', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+
+            $table->string('username');
+
+            $table->string('email')->unique();
+
+            $table->string('password');
+
+            $table->string('nik');
+
+            $table->string('no_tlp');
+
+            $table->string('alamat');
+
+            $table->string('remember_token')->nullable();
+
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
