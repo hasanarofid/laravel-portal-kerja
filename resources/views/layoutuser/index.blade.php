88eb4eb7d9b177e3386ecb16b72b8b26ad462685
Mahrus13 <muhammadmahrus1501@gmail.com>
2024-05-09 18:19:00 +0700

    tampilan users profil

--- resources/views/layoutuser/index.blade.php
+++ resources/views/layoutuser/index.blade.php
@@ -29,13 +29,27 @@
     <link rel="stylesheet" href="{{ asset('bower_components/bootstrap-daterangepicker/daterangepicker.css') }}">
     <!-- bootstrap wysihtml5 - text editor -->
     <link rel="stylesheet" href="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+    <!-- daterange picker -->
+    <link rel="stylesheet" href="{{ asset('bower_components/bootstrap-daterangepicker/daterangepicker.css') }}">
+    <!-- bootstrap datepicker -->
+    <link rel="stylesheet"
+        href="{{ asset('bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
+    <!-- iCheck for checkboxes and radio inputs -->
+    <link rel="stylesheet" href="{{ asset('plugins/iCheck/all.css') }}">
+    <!-- Bootstrap Color Picker -->
+    <link rel="stylesheet"
+        href="{{ asset('bower_components/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css') }}">
+    <!-- Bootstrap time Picker -->
+    <link rel="stylesheet" href="{{ asset('plugins/timepicker/bootstrap-timepicker.min.css') }}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('bower_components/select2/dist/css/select2.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="{{ asset('dist/css/skins/_all-skins.min.css') }}">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+
 
     <!-- Google Font -->
     <link rel="stylesheet"
@@ -47,17 +61,19 @@
 
         <header class="main-header">
             <!-- Logo -->
-            <a href="index2.html" class="logo">
-                <!-- mini logo for sidebar mini 50x50 pixels -->
-                <span class="logo-mini"><b>STKO</b></span>
-                <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><img src="{{ asset('img/logodisnaker.PNG') }}" class="card-img-top"
-                        style="position: relative;z-index: 1;"></span>
-            </a>
+            @if (session('role_id') == 3)
+                <a href="{{ route('users.home') }}" class="logo">
+            @endif
+                    <!-- mini logo for sidebar mini 50x50 pixels -->
+                    <span class="logo-mini"><b>STKO</b></span>
+                    <!-- logo for regular state and mobile devices -->
+                    <span class="logo-lg"><img src="{{ asset('img/logodisnaker.PNG') }}" class="card-img-top"
+                            style="position: relative;z-index: 1;"></span>
+                </a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top">
                 <!-- Sidebar toggle button-->
-                <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                <a href="" class="sidebar-toggle" data-toggle="push-menu" role="button">
                     <span class="sr-only">Toggle navigation</span>
                 </a>
 
@@ -66,30 +82,35 @@
                         <!-- User Account: style can be found in dropdown.less -->
                         <li class="dropdown user user-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
+                                <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="user-image"
+                                    alt="User Image">
                                 @if (session('role_id') == 3)
-                                  <span class="hidden-xs">{{ session('pencaker_name') }}</span>
+                                    <span class="hidden-xs">{{ session('pencaker_name') }}</span>
                                 @endif
                             </a>
                             <ul class="dropdown-menu">
                                 <!-- User image -->
                                 <li class="user-header">
-                                    <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+                                    <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle"
+                                        alt="User Image">
 
                                     <p>
-                                        {{session('pencaker_name')}}
+                                        {{ session('pencaker_name') }}
                                         @if (session('role_id') == 3)
-                                        <small>{{session('alamat')}}</small>
+                                            <small>{{ session('alamat') }}</small>
                                         @endif
                                     </p>
                                 </li>
                                 <!-- Menu Footer-->
                                 <li class="user-footer">
                                     <div class="pull-left">
-                                        <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                        @if (session('role_id') == 3)
+                                            <a href="{{ route('users.profil') }}" class="btn btn-default btn-flat">Profil</a>
+                                        @endif
                                     </div>
                                     <div class="pull-right">
-                                        <a href="{{ route('users.logout') }}" class="btn btn-default btn-flat">Sign out</a>
+                                        <a href="{{ route('users.logout') }}" class="btn btn-default btn-flat">Sign
+                                            out</a>
                                     </div>
                                 </li>
                             </ul>
@@ -128,7 +149,7 @@
                 @if (session('role_id') == 3)
                     <ul class="sidebar-menu" data-widget="tree">
                         <li class="header">MAIN NAVIGATION</li>
-                        <li><a href="#"><i class="fa fa-home"></i> <span>Home</span></a></li>
+                        <li><a href="{{ route('users.home') }}"><i class="fa fa-home"></i> <span>Home</span></a></li>
                         <li><a href="#"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
                         <li class="treeview">
                             <a href="#">
@@ -382,35 +403,102 @@
     <script>
         $.widget.bridge('uibutton', $.ui.button);
     </script>
+    <!-- jQuery 3 -->
+    <script src="{{asset ('bower_components/jquery/dist/jquery.min.js') }}"></script>
     <!-- Bootstrap 3.3.7 -->
-    <script src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-    <!-- Morris.js charts -->
-    <script src="{{ asset('bower_components/raphael/raphael.min.js') }}"></script>
-    <script src="{{ asset('bower_components/morris.js/morris.min.js') }}"></script>
-    <!-- Sparkline -->
-    <script src="{{ asset('bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') }}"></script>
-    <!-- jvectormap -->
-    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-    <!-- jQuery Knob Chart -->
-    <script src="{{ asset('bower_components/jquery-knob/dist/jquery.knob.min.js') }}"></script>
-    <!-- daterangepicker -->
-    <script src="{{ asset('bower_components/moment/min/moment.min.js') }}"></script>
-    <script src="{{ asset('bower_components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
-    <!-- datepicker -->
-    <script src="{{ asset('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
-    <!-- Bootstrap WYSIHTML5 -->
-    <script src="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
-    <!-- Slimscroll -->
-    <script src="{{ asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <script src="{{asset ('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- Select2 -->
+    <script src="{{asset ('bower_components/select2/dist/js/select2.full.min.js') }}"></script>
+    <!-- InputMask -->
+    <script src="{{asset ('plugins/input-mask/jquery.inputmask.js') }}"></script>
+    <script src="{{asset ('plugins/input-mask/jquery.inputmask.date.extensions.js') }}"></script>
+    <script src="{{asset ('plugins/input-mask/jquery.inputmask.extensions.js') }}"></script>
+    <!-- date-range-picker -->
+    <script src="{{asset ('bower_components/moment/min/moment.min.js') }}"></script>
+    <script src="{{asset ('bower_components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+    <!-- bootstrap datepicker -->
+    <script src="{{asset ('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+    <!-- bootstrap color picker -->
+    <script src="{{asset ('bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js') }}"></script>
+    <!-- bootstrap time picker -->
+    <script src="{{asset ('plugins/timepicker/bootstrap-timepicker.min.js') }}"></script>
+    <!-- SlimScroll -->
+    <script src="{{asset ('bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <!-- iCheck 1.0.1 -->
+    <script src="{{asset ('plugins/iCheck/icheck.min.js') }}"></script>
     <!-- FastClick -->
-    <script src="{{ asset('bower_components/fastclick/lib/fastclick.js') }}"></script>
+    <script src="{{asset ('bower_components/fastclick/lib/fastclick.js') }}"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
+    <script src="{{asset ('dist/js/adminlte.min.js') }}"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="{{ asset('dist/js/demo.js') }}"></script>
+    <script src="{{asset ('dist/js/demo.js') }}"></script>
+    <script>
+        $(function () {
+          //Initialize Select2 Elements
+          $('.select2').select2()
+      
+          //Datemask dd/mm/yyyy
+          $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+          //Datemask2 mm/dd/yyyy
+          $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+          //Money Euro
+          $('[data-mask]').inputmask()
+      
+          //Date range picker
+          $('#reservation').daterangepicker()
+          //Date range picker with time picker
+          $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
+          //Date range as a button
+          $('#daterange-btn').daterangepicker(
+            {
+              ranges   : {
+                'Today'       : [moment(), moment()],
+                'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+                'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+              },
+              startDate: moment().subtract(29, 'days'),
+              endDate  : moment()
+            },
+            function (start, end) {
+              $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+            }
+          )
+      
+          //Date picker
+          $('#datepicker').datepicker({
+            autoclose: true
+          })
+      
+          //iCheck for checkbox and radio inputs
+          $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+            checkboxClass: 'icheckbox_minimal-blue',
+            radioClass   : 'iradio_minimal-blue'
+          })
+          //Red color scheme for iCheck
+          $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+            checkboxClass: 'icheckbox_minimal-red',
+            radioClass   : 'iradio_minimal-red'
+          })
+          //Flat red color scheme for iCheck
+          $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+            checkboxClass: 'icheckbox_flat-green',
+            radioClass   : 'iradio_flat-green'
+          })
+      
+          //Colorpicker
+          $('.my-colorpicker1').colorpicker()
+          //color picker with addon
+          $('.my-colorpicker2').colorpicker()
+      
+          //Timepicker
+          $('.timepicker').timepicker({
+            showInputs: false
+          })
+        })
+      </script>
 </body>
 
 </html>
